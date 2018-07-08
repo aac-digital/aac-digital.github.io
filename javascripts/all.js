f5fafd5368e65a1b2f44eb737f31740dfd0a8336
Adam Haris <iamadamharis@gmail.com>
2018-07-08 17:50:04 +0200

    Site updated to 0e14f1e

--- javascripts/all.js
+++ javascripts/all.js
@@ -13418,19 +13418,4 @@ return jQuery;
 
 +(function(window, document, undefined){
   "use strict";
-
-  var owl = $('.owl-carousel');
-  owl.owlCarousel({
-    items: 1,
-    loop: true,
-    center: true
-  });
-
-  $('.prev-arrow').click(function() {
-    owl.trigger('prev.owl.carousel');
-  });
-
-  $('.next-arrow').click(function() {
-    owl.trigger('next.owl.carousel');
-  });
 })(this, this.document);
